194e0481f4c9481d5517504a0ddcfe82e58ce177
maya-sivan <mayasivannj@gmail.com>
2025-07-13 20:21:07 -0400

    chore: add error boundary

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+	type Query,
+	QueryClient,
+	QueryClientProvider,
+} from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "antd/dist/reset.css";
 import "./app.css";
+import { notification } from "antd";
 import { Provider as JotaiProvider } from "jotai";
 import {
 	AppLayout,
@@ -13,10 +19,20 @@ import {
 	QueryPage,
 	RecipesPage,
 } from "./components";
+import { ErrorFallback } from "./components/ErrorFallback";
 import { NotFoundPage } from "./components/NotFoundPage";
 import { ROUTES } from "./constants";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			throwOnError: true,
+		},
+		mutations: {
+			throwOnError: true,
+		},
+	},
+});
 
 const router = createBrowserRouter([
 	{
@@ -37,9 +53,11 @@ const root = ReactDOM.createRoot(container);
 root.render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<JotaiProvider store={globalAtomStore}>
-				<RouterProvider router={router} />
-			</JotaiProvider>
+			<ErrorBoundary FallbackComponent={ErrorFallback}>
+				<JotaiProvider store={globalAtomStore}>
+					<RouterProvider router={router} />
+				</JotaiProvider>
+			</ErrorBoundary>
 		</QueryClientProvider>
 	</React.StrictMode>,
 );
